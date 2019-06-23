52b7106dbd7d52ea4ba61b3fb811ee7f30a8a4ff
mlambers <mlambers@mail.ccsf.edu>
2019-06-23 21:03:35 +0700

    Added Brood

--- MorphBug.lua
+++ MorphBug.lua
@@ -2,16 +2,19 @@ local MorphBug = {}
 MorphBug.OptionEnable = Menu.AddOption({"mlambers", "Morph Alce Bug"}, "1. Enable", "")
 MorphBug.OptionKey = Menu.AddKeyOption({"mlambers", "Morph Alce Bug"}, "2. Key to execute Alchemist", Enum.ButtonCode.KEY_D)
 MorphBug.OptionKeyTiny = Menu.AddKeyOption({"mlambers", "Morph Alce Bug"}, "3. Key to execute Tiny", Enum.ButtonCode.KEY_D)
+MorphBug.OptionKeyBrood = Menu.AddKeyOption({"mlambers", "Morph Alce Bug"}, "4. Key to execute Brood", Enum.ButtonCode.KEY_D)
 
 local Ab = nil
 local Ab2 = nil
+local Ab3 = nil
 local MyHero = nil
 local Victim = nil
 
 function MorphBug.OnScriptLoad()
     MyHero = nil
 	Ab = nil
-	Ab2 = nil
+    Ab2 = nil
+    Ab3 = nil
     Victim = nil
 end
 
@@ -55,13 +58,35 @@ function MorphBug.Tiny()
 	end
 end
 
+function MorphBug.Brood()
+    if Ab3 == nil or not Abilities.Contains(Ab3) or Entity.GetOwner(Ab3) ~= MyHero then
+        if Ability.GetName( NPC.GetAbilityByIndex(MyHero, 1) ) == 'broodmother_spin_web' then
+			Ab3 = NPC.GetAbilityByIndex(MyHero, 1)
+        end
+    else
+        -- Refresh Web
+        if 
+            NPC.HasModifier(MyHero, "modifier_morphling_replicate")
+            and Ability.GetName( NPC.GetAbilityByIndex(MyHero, 1) ) == 'broodmother_spin_web'
+            and Ability.GetLevelSpecialValueFor(Ab3, "count") ~= Ability.GetLevelSpecialValueFor(NPC.GetAbilityByIndex(MyHero, 1), "count")
+        then
+            Ab3 = NPC.GetAbilityByIndex(MyHero, 1)
+        end
+
+        if  Menu.IsKeyDownOnce(MorphBug.OptionKeyBrood) and Input.IsInputCaptured() == false  then
+            Ability.CastPosition(Ab3, Input.GetWorldCursorPos())
+        end
+	end
+end
+
 function MorphBug.OnUpdate()
     if Menu.IsEnabled(MorphBug.OptionEnable) == false then return end
 
     if MyHero == nil or MyHero ~= Heroes.GetLocal() then
         MyHero = Heroes.GetLocal()
 		Ab = nil
-		Ab2 = nil
+        Ab2 = nil
+        Ab3 = nil
         Victim = nil
         Console.Print("Script init MorphBug.lua")
         return
@@ -71,7 +96,8 @@ function MorphBug.OnUpdate()
    
     if Entity.IsAlive(MyHero) then
 		MorphBug.Alchemist()
-		MorphBug.Tiny()
+        MorphBug.Tiny()
+        MorphBug.Brood()
     end
 end
 
